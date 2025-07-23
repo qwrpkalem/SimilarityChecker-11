874080882b2bef158068fc12ded99f459b3a3c56
jk <qwrpkalem@naver.com>
2025-07-23 15:14:23 +0900

    [feature] page 1

--- similaritycheck.cpp
+++ similaritycheck.cpp
@@ -1,3 +1,13 @@
+#include <string>
+
+using namespace std;
+
 class SimilarityCheck {
 public:
+	int lengthscore(string a, string b)
+	{
+		if (a == b) {
+			return 60;
+		}
+	}
 };
--- similaritycheck_test.cpp
+++ similaritycheck_test.cpp
@@ -2,6 +2,14 @@
 #include "gmock/gmock.h"
 #include "SimilarityCheck.cpp"
 
+
+TEST(LCTS, CheckStrSameLength)
+{
+	SimilarityCheck SC;
+	int actual = SC.lengthscore("ABC", "ABC");
+	EXPECT_EQ(actual, 60);
+}
+
 int main()
 {
 	::testing::InitGoogleMock();
